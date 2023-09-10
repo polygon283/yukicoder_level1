b842d21cbbc5681d6fbefaa5b561c43cf1f713c4
polygon283 <143920381+polygon283@users.noreply.github.com>
2023-09-10 21:09:57 +0900

    Add files via upload

--- No_1168.py
+++ No_1168.py
@@ -1,4 +1,11 @@
 N = input()
-split_N = list(map(int,N.split()))
 
-print(split_N)
+def Digit_Sum_Sequence(N): #a100を求める関数
+    for _ in range(100):
+        sum = 0
+        for Digit in str(N):
+            sum += int(Digit)
+        N = sum
+    return N
+
+print(Digit_Sum_Sequence(N))
